3b4faaa655685e2fd630222567b054f7ae5c9f78
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-09-05 23:54:36 +0530

    error resolved

--- server.js
+++ server.js
@@ -68,10 +68,18 @@ const log = console.log;
 // initialize http server, socket.io and port number
 const express = require("express");
 const app = express();
-const http = require("http").createServer();
+const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.use("/", express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+
+// const express = require("express");
+// const app = express();
+// const http = require("http").Server(app);
+// const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
 
 io.on("connection", (socket) => {
   log("connected");
@@ -84,5 +92,5 @@ io.on("disconnect", (evt) => {
   log("some people left");
 });
 
-const port = 3000;
+// const port = 3000;
 http.listen(port, () => log(`server started on: http://localhost:${port}`));
